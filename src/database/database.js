c28e41d2507125a98b843f38d6dd3cc5b7d5148f
¨capinf¨ <capinf@gmail.com>
2023-07-31 10:13:03 -0300

    changin environment variables

--- src/database/database.js
+++ src/database/database.js
@@ -7,8 +7,7 @@ const mysqlConeccion= mysql.createConnection({
     host:process.env.DB_HOST,
     user: process.env.DB_USER,
     password:process.env.DB_PASSWORD,
-    database:process.env.DB_NAME,
-    database:process.env.DB_PORT
+    database:process.env.DB_NAME
 });
 
 mysqlConeccion.connect(function(err){
