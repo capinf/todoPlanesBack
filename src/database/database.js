8fd7e01f7662069e1a50b8514339fbdfcf5843d7
¨capinf¨ <capinf@gmail.com>
2023-07-31 08:01:57 -0300

    Making changes for deploy on Render

--- src/database/database.js
+++ src/database/database.js
@@ -1,10 +1,13 @@
 const mysql = require('mysql');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const mysqlConeccion= mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'todoplanesweb'
+    
+    host:process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_NAME
 });
 
 mysqlConeccion.connect(function(err){
