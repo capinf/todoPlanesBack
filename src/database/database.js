7dbdfc52f998311d36ad84f8d8f37a544b9db65e
¨capinf¨ <capinf@gmail.com>
2023-08-04 16:03:42 -0300

    Agregando pool connection

--- src/database/database.js
+++ src/database/database.js
@@ -1,22 +1,26 @@
-const mysql = require('mysql');
+const mysql = require('mysql2/promise'); // Importamos mysql2 en modo promesa
 const dotenv = require('dotenv');
 dotenv.config();
 
-const mysqlConeccion= mysql.createConnection({
-    
-    host:process.env.DB_HOST,
+const mysqlConeccion = mysql.createPool({ // Creamos el pool de conexiones
+    host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password:process.env.DB_PASSWORD,
-    database:process.env.DB_NAME
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10, // Puedes ajustar este valor según tus necesidades
+    queueLimit: 0 // No hay límite para el tamaño de la cola
 });
 
-mysqlConeccion.connect(function(err){
-    if(err){
-        console.log('mi error es', err);
-        return;
-    }else{
-        console.log('Mi conexion se realizo correctamente');
-    }
-})
+mysqlConeccion.getConnection()
+    .then(connection => {
+        console.log('Mi conexión se realizó correctamente');
+        // Si necesitas ejecutar alguna consulta inicial o realizar alguna tarea después de obtener la conexión, puedes hacerlo aquí.
+        // connection.query(...)
+        // connection.release(); // Importante liberar la conexión cuando hayas terminado con ella.
+    })
+    .catch(error => {
+        console.error('Mi error es', error);
+    });
 
-module.exports= mysqlConeccion;
+module.exports = mysqlConeccion; // Exportamos el pool en lugar de la conexión única
---
+++ src/database/databaseBack.js
@@ -0,0 +1,22 @@
+const mysql = require('mysql');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const mysqlConeccion= mysql.createConnection({
+    
+    host:process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_NAME
+});
+
+mysqlConeccion.connect(function(err){
+    if(err){
+        console.log('mi error es', err);
+        return;
+    }else{
+        console.log('Mi conexion se realizo correctamente');
+    }
+})
+
+module.exports= mysqlConeccion;
