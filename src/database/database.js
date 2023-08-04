e07ec95b45d702bb03d81fcbc625fc157a5376ad
¨capinf¨ <capinf@gmail.com>
2023-08-04 16:18:15 -0300

    Adding PoolConnection to avoid crashes

--- src/database/database.js
+++ src/database/database.js
@@ -1,26 +1,18 @@
-const mysql = require('mysql2/promise'); // Importamos mysql2 en modo promesa
+const mysql = require('mysql');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const mysqlConeccion = mysql.createPool({ // Creamos el pool de conexiones
-    host: process.env.DB_HOST,
+// Create a connection pool instead of a single connection
+const mysqlConeccion = mysql.createPool({
+    connectionLimit: 10, // You can adjust the connection limit based on your needs
+    host:process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10, // Puedes ajustar este valor según tus necesidades
-    queueLimit: 0 // No hay límite para el tamaño de la cola
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_NAME
 });
 
-mysqlConeccion.getConnection()
-    .then(connection => {
-        console.log('Mi conexión se realizó correctamente');
-        // Si necesitas ejecutar alguna consulta inicial o realizar alguna tarea después de obtener la conexión, puedes hacerlo aquí.
-        // connection.query(...)
-        // connection.release(); // Importante liberar la conexión cuando hayas terminado con ella.
-    })
-    .catch(error => {
-        console.error('Mi error es', error);
-    });
 
-module.exports = mysqlConeccion; // Exportamos el pool en lugar de la conexión única
+// You don't need to explicitly connect to the database with a pool
+
+// Export the connection pool
+module.exports = mysqlConeccion;
