ae35d72e5624eff418264c0022554b8468f94483
Nahuel Cabrera Zembrunski <113146310+NahuelCabreraZ@users.noreply.github.com>
2023-08-23 21:20:59 -0300

    Imgpath correcto en router

--- src/router/router.js
+++ src/router/router.js
@@ -316,7 +316,7 @@ router.delete('/eliminar_publicacion/:nombrePlan',(req, res)=>{
         console.log('console log en router js req body: ', req.body);
         console.log('console log en router js req body: ', req.file)
         const { nombrePlan, tipoPlan, precio, cantidadCuotas, adjudicado, anioInicio, localidad, telefono, rolform } = req.body
-        const imgPath = req.file ? `uploads/${req.file.filename}` : 'img/imgDefault.png';
+        const imgPath = req.file ? `https://todoplanesback.onrender.com/uploads/${req.file.filename}` : 'img/imgDefault.png';
         console.log('el img path es ', imgPath)
     
                 let query=`INSERT INTO formulario (nombrePlan, tipoPlan, precio, cantidadCuotas, adjudicado, anioInicio, localidad, telefono, imgPath, rolform, fecha) 
